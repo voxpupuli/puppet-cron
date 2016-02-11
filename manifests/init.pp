530646bfb77062a3b50961f3e5bb3cd03c8eec8d
Abel Paz <abelfrancisco.paz@ciemat.es>
2016-02-11 11:08:41 +0100

    Added possibility to add jobs from hiera
    init.pp improved to automatically create cron jobs dinamically from YAML
    files importing them via Hiera.

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,5 +21,36 @@ class cron (
     package_ensure => $package_ensure,
   }
 
+  # Create jobs from hiera
+  $cron_job=hiera_hash('cron::job','')
+  if $cron_job != '' {
+    create_resources('cron::job',$cron_job)
+  }
+  
+  $cron_job_multiple=hiera_hash('cron::job::multiple','')
+  if $cron_job_multiple != '' {
+    create_resources('cron::job::multiple',$cron_job_multiple)
+  }
+
+  $cron_job_hourly=hiera_hash('cron::hourly','')
+  if $cron_job_hourly != '' {
+    create_resources('cron::hourly',$cron_job_hourly)
+  }
+
+  $cron_job_daily=hiera_hash('cron::daily','')
+  if $cron_job_daily != '' {
+    create_resources('cron::daily',$cron_job_daily)
+  }
+
+  $cron_job_weekly=hiera_hash('cron::weekly','')
+  if $cron_job_weekly != '' {
+    create_resources('cron::weekly',$cron_job_weekly)
+  }
+
+  $cron_job_monthly=hiera_hash('cron::monthly','')
+  if $cron_job_monthly != '' {
+    create_resources('cron::monthly',$cron_job_monthly)
+  }
+
 }
 
