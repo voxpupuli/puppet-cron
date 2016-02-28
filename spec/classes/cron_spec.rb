0f19fa4aeb3c591a322c6c614d723018a878ec49
Roman Mueller <roman.mueller@gmail.com>
2016-02-28 09:51:48 +0100

    Add tests for cron::manage_package => false

--- spec/classes/cron_spec.rb
+++ spec/classes/cron_spec.rb
@@ -4,3 +4,19 @@ describe 'cron' do
   it { should contain_class( 'cron::install' ) }
 end
 
+describe 'cron' do
+  let( :params ) {{ :manage_package => false,
+                    :package_ensure => 'cron', }}
+
+  it { is_expected.to_not contain_class( 'cron::install' ) }
+  it { is_expected.to_not contain_package( 'cron' ) }
+end
+
+describe 'cron' do
+  let( :params ) {{ :manage_package => true,
+                    :package_ensure => 'cron', }}
+
+  it { should contain_class( 'cron::install' ) }
+  it { should contain_package( 'cron' ) }
+end
+
