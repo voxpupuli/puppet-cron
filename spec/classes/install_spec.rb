30024ee3573ff61783bdedc0e3870f9c39e13eb6
Roman Mueller <roman.mueller@gmail.com>
2016-03-04 19:45:08 +0100

    Add more tests for package selector

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -1,8 +1,80 @@
 require 'spec_helper'
 
 describe 'cron::install' do
-  it do
-    should contain_package( 'cron' ).with( 'ensure' => 'installed' )
+  context 'default' do
+    it do
+      should contain_package( 'cron' ).with( 'ensure' => 'installed', 'name' => 'cron' )
+    end
   end
+
+  context 'CentOS 5' do
+    let :facts do
+      {
+        :osfamily                  => 'RedHat',
+        :operatingsystem           => 'CentOS',
+        :operatingsystemmajrelease => '5',
+      }
+    end
+    it { should contain_class( 'cron::install' ) }
+    it { should contain_package( 'cron' ).with(
+      'name' => 'vixie-cron'
+      )
+    }
+  end
+
+  context 'CentOS 6' do
+    let :facts do
+      {
+        :osfamily                  => 'RedHat',
+        :operatingsystem           => 'CentOS',
+        :operatingsystemmajrelease => '6',
+      }
+    end
+    it { should contain_class( 'cron::install' ) }
+    it { should contain_package( 'cron' ).with(
+      'name' => 'cronie'
+      )
+    }
+  end
+
+  context 'Gentoo' do
+    let :facts do
+      {
+        :operatingsystem           => 'Gentoo',
+      }
+    end
+    it { should contain_class( 'cron::install' ) }
+    it { should contain_package( 'cron' ).with(
+      'name' => 'sys-process/vixie-cron'
+      )
+    }
+  end
+
+  context 'Ubuntu' do
+    let :facts do
+      {
+        :operatingsystem           => 'Ubuntu',
+      }
+    end
+    it { should contain_class( 'cron::install' ) }
+    it { should contain_package( 'cron' ).with(
+      'name' => 'cron'
+      )
+    }
+  end
+
+  context 'Debian' do
+    let :facts do
+      {
+        :operatingsystem           => 'Debian',
+      }
+    end
+    it { should contain_class( 'cron::install' ) }
+    it { should contain_package( 'cron' ).with(
+      'name' => 'cron'
+      )
+    }
+  end
+
 end
 
