voxpupuli/puppet-cron

commit bc5aad7a0f523c114f6630c8d586dfcabf5ec01e

Add cron.deny/allow and refactored for Puppet4 (#27)
* Restore accidentally removed path.

* Add type-hinting to all manifest parameters.

* Refactored for Puppet 4 and added cron.allow/deny

* Feature: add management of /etc/cron.allow and /etc/cron.deny
* Enhancement: Replaced hiera_hash() with lookup() calls
* Enhancement: Replaced params.pp with in-module data (Hiera 5)
* Enhancement: Replaced create_resources with iterators
* Enhancement: Replaced anchor pattern with contain
* Metadata: Changed compatibility to Puppet >4.9.0 <6.0.0

* added README update for new params

* fixed syntax issue with template

* added missing users_deny from lookup_options

manifests/init.pp
@@ -34,53 +34,98 @@
 #   }
 #
 class cron (
-  $manage_package = true,
-  $manage_service = true,
-  $service_ensure = 'running',
-  $service_enable = true,
-  $service_name   = $::cron::params::service_name,
-  $package_ensure = 'installed',
-  $package_name   = $::cron::params::package_name,
-) inherits cron::params {
+  String[1]      $service_name,
+  String[1]      $package_name,
+  Boolean        $manage_package = true,
+  Boolean        $manage_service = true,
+  Variant[
+    Boolean,
+    Enum[
+      'running',
+      'stopped',
+    ]
+  ]              $service_ensure = 'running',
+  Variant[
+    Boolean,
+    Enum[
+      'manual',
+      'mask',
+    ]
+  ]              $service_enable = true,
+  String[1]      $package_ensure = 'installed',
+  Array[String]  $users_allow    = [],
+  Array[String]  $users_deny     = [],
+  Boolean        $manage_users_allow = false,
+  Boolean        $manage_users_deny  = false,
+) {
 
-  validate_bool($manage_package, $manage_service, $service_enable)
+  contain '::cron::install'
+  contain '::cron::service'
 
-  include ::cron::install
-  include ::cron::service
+  Class['cron::install'] -> Class['cron::service']
 
-  anchor { 'cron::start': }
-  -> Class['cron::install']
-  -> Class['cron::service']
-  -> anchor { 'cron::end': }
+  # Manage cron.allow and cron.deny
+  if $manage_users_allow {
+    file { '/etc/cron.allow':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      content => epp('cron/users.epp', { 'users' => $users_allow }),
+    }
+  }
+
+  if $manage_users_deny {
+    file { '/etc/cron.deny':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      content => epp('cron/users.epp', { 'users' => $users_deny }),
+    }
+  }
+
 
   # Create jobs from hiera
-  $cron_job = hiera_hash('cron::job', undef)
-  if $cron_job {
-    create_resources('cron::job',$cron_job)
+
+  $cron_job = lookup('cron::job', Optional[Hash], 'hash', {})
+  $cron_job.each | String $t, Hash $params | {
+    cron::job { $t:
+      * => $params
+    }
   }
 
-  $cron_job_multiple = hiera_hash('cron::job::multiple', undef)
-  if $cron_job_multiple {
-    create_resources('cron::job::multiple', $cron_job_multiple)
+  $cron_job_multiple = lookup('cron::job::multiple', Optional[Hash], 'hash', {})
+  $cron_job_multiple.each | String $t, Hash $params | {
+    cron::job::multiple { $t:
+      * => $params
+    }
   }
 
-  $cron_job_hourly = hiera_hash('cron::hourly', undef)
-  if $cron_job_hourly {
-    create_resources('cron::hourly', $cron_job_hourly)
+  $cron_job_hourly = lookup('cron::job::hourly', Optional[Hash], 'hash', {})
+  $cron_job_hourly.each | String $t, Hash $params | {
+    cron::job::hourly { $t:
+      * => $params
+    }
   }
 
-  $cron_job_daily = hiera_hash('cron::daily', undef)
-  if $cron_job_daily {
-    create_resources('cron::daily', $cron_job_daily)
+  $cron_job_daily = lookup('cron::job::daily', Optional[Hash], 'hash', {})
+  $cron_job_daily.each | String $t, Hash $params | {
+    cron::job::daily { $t:
+      * => $params
+    }
   }
 
-  $cron_job_weekly = hiera_hash('cron::weekly', undef)
-  if $cron_job_weekly {
-    create_resources('cron::weekly', $cron_job_weekly)
+  $cron_job_weekly = lookup('cron::job::weekly', Optional[Hash], 'hash', {})
+  $cron_job_weekly.each | String $t, Hash $params | {
+    cron::job::weekly { $t:
+      * => $params
+    }
   }
 
-  $cron_job_monthly = hiera_hash('cron::monthly', undef)
-  if $cron_job_monthly {
-    create_resources('cron::monthly', $cron_job_monthly)
+  $cron_job_monthly = lookup('cron::job::monthly', Optional[Hash], 'hash', {})
+  $cron_job_monthly.each | String $t, Hash $params | {
+    cron::job::monthly { $t:
+      * => $params
+    }
   }
+
 }

.travis.yml
@@ -4,15 +4,15 @@ language: ruby
 script: "bundle exec rake validate && bundle exec rake lint && bundle exec rake spec SPEC_OPTS='--format documentation'"
 matrix:
   include:
-  - rvm: 2.1.6
-    env: PUPPET_GEM_VERSION="~> 4.0" STRICT_VARIABLES="yes"
-  - rvm: 2.1.8
-    env: PUPPET_GEM_VERSION="~> 4.0" STRICT_VARIABLES="yes"
   - rvm: 2.1.9
-    env: PUPPET_GEM_VERSION="~> 4.0" STRICT_VARIABLES="yes"
-  - rvm: 2.2.4
-    env: PUPPET_GEM_VERSION="~> 4.0" STRICT_VARIABLES="yes"
-  - rvm: 2.3.0
-    env: PUPPET_GEM_VERSION="~> 4.0" STRICT_VARIABLES="yes"
-  - rvm: 2.3.1
-    env: PUPPET_GEM_VERSION="~> 4.0" STRICT_VARIABLES="yes"
+    env: PUPPET_GEM_VERSION="~> 4.9.1" STRICT_VARIABLES="yes"
+  - rvm: 2.1.9
+    env: PUPPET_GEM_VERSION="~> 4.10.0" STRICT_VARIABLES="yes"
+  - rvm: 2.4.1
+    env: PUPPET_GEM_VERSION="~> 5.0.0" STRICT_VARIABLES="yes"
+  - rvm: 2.4.1
+    env: PUPPET_GEM_VERSION="~> 5.1.0" STRICT_VARIABLES="yes"
+  - rvm: 2.4.1
+    env: PUPPET_GEM_VERSION="~> 5.2.0" STRICT_VARIABLES="yes"
+  - rvm: 2.4.1
+    env: PUPPET_GEM_VERSION="~> 5" STRICT_VARIABLES="yes"

data/common.yaml
@@ -1,0 +1,10 @@
+---
+
+cron::package_name: cron
+cron::service_name: cron
+
+lookup_options:
+  cron::users_allow:
+    merge: unique
+  cron::users_deny:
+    merge: unique

data/os/Gentoo.yaml
@@ -1,0 +1,2 @@
+---
+cron::package_name: virtual/cron

data/os/RedHat.yaml
@@ -1,0 +1,3 @@
+---
+cron::package_name: cronie
+cron::service_name: crond

data/os/RedHat/5.yaml
@@ -1,0 +1,3 @@
+---
+cron::package_name: vixie-cron
+

hiera.yaml
@@ -1,0 +1,14 @@
+---
+version: 5
+defaults:
+  datadir: data
+  data_hash: yaml_data
+hierarchy:
+
+  - name: "Operating system"
+    paths: 
+      - "os/%{facts.os.family}/%{facts.os.release.major}.yaml"
+      - "os/%{facts.os.family}.yaml"
+
+  - name: "common"
+    path: "common.yaml"

manifests/daily.pp
@@ -35,14 +35,14 @@
 #   }
 #
 define cron::daily (
-  $command     = undef,
-  $ensure      = 'present',
-  $minute      = 0,
-  $hour        = 0,
-  $environment = [],
-  $user        = 'root',
-  $mode        = '0644',
-  $description = undef,
+  Optional[String[1]]        $command     = undef,
+  Enum['absent','present']   $ensure      = 'present',
+  Variant[Integer,String[1]] $minute      = 0,
+  Variant[Integer,String[1]] $hour        = 0,
+  Array[String]              $environment = [],
+  String[1]                  $user        = 'root',
+  String[4,4]                $mode        = '0644',
+  Optional[String]           $description = undef,
 ) {
 
   cron::job { $title:

commit 61515cf5a9ef67e0cba531f2af3ad09b04101295

Fix hiera lookup regression
The refactoring in https://github.com/voxpupuli/puppet-cron/pull/27
broke hiera lookups done by the base class.

manifests/init.pp
@@ -100,30 +100,30 @@ class cron (
     }
   }
 
-  $cron_job_hourly = lookup('cron::job::hourly', Optional[Hash], 'hash', {})
-  $cron_job_hourly.each | String $t, Hash $params | {
-    cron::job::hourly { $t:
+  $cron_hourly = lookup('cron::hourly', Optional[Hash], 'hash', {})
+  $cron_hourly.each | String $t, Hash $params | {
+    cron::hourly { $t:
       * => $params,
     }
   }
 
-  $cron_job_daily = lookup('cron::job::daily', Optional[Hash], 'hash', {})
-  $cron_job_daily.each | String $t, Hash $params | {
-    cron::job::daily { $t:
+  $cron_daily = lookup('cron::daily', Optional[Hash], 'hash', {})
+  $cron_daily.each | String $t, Hash $params | {
+    cron::daily { $t:
       * => $params,
     }
   }
 
-  $cron_job_weekly = lookup('cron::job::weekly', Optional[Hash], 'hash', {})
-  $cron_job_weekly.each | String $t, Hash $params | {
-    cron::job::weekly { $t:
+  $cron_weekly = lookup('cron::weekly', Optional[Hash], 'hash', {})
+  $cron_weekly.each | String $t, Hash $params | {
+    cron::weekly { $t:
       * => $params,
     }
   }
 
-  $cron_job_monthly = lookup('cron::job::monthly', Optional[Hash], 'hash', {})
-  $cron_job_monthly.each | String $t, Hash $params | {
-    cron::job::monthly { $t:
+  $cron_monthly = lookup('cron::monthly', Optional[Hash], 'hash', {})
+  $cron_monthly.each | String $t, Hash $params | {
+    cron::monthly { $t:
       * => $params,
     }
   }